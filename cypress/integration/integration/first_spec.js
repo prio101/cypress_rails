2ddeeaa1b206fed1748d464df4fa78b106236a1a
prio101 <miprio101@gmail.com>
2021-06-06 14:17:17 +0600

    update 12

--- cypress/integration/integration/first_spec.js
+++ cypress/integration/integration/first_spec.js
@@ -1,6 +1,6 @@
 describe('first spec', ()=>{
   it('Should visit the root page', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://127.0.0.1:3000');
     cy.get('h1').should('have.text', 'This is an App Created for the Cypress Testing on Github.')
   })
 })
